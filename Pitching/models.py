88a60a681828453162e4b7a9ad428d0066fd1e24
Pip Bbaale <namugera.philip@gmail.com>
2020-05-05 22:44:22 +0300

    model manager

--- Pitching/models.py
+++ Pitching/models.py
@@ -5,7 +5,6 @@ from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-    <!!!>
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
