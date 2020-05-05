002a62afb770111d853b5d313f3db00f9acb8a05
Pip Bbaale <namugera.philip@gmail.com>
2020-05-05 22:47:58 +0300

    _init_ apps

--- Pitching/__init__.py
+++ Pitching/__init__.py
@@ -15,7 +15,6 @@ app.config['UPLOADED_PHOTOS_DEST'] ='Pitching/static/photos'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-https://github.com/scrupycoco/PitchPip.git
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
