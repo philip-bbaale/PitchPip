1762f53d13ed8662f11b378f114a2b9a89c6e201
Pip Bbaale <namugera.philip@gmail.com>
2020-05-05 22:47:11 +0300

    _init_ imports

--- Pitching/__init__.py
+++ Pitching/__init__.py
@@ -11,17 +11,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOADED_PHOTOS_DEST'] ='Pitching/static/photos'
+https://github.com/scrupycoco/PitchPip.git
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+https://github.com/scrupycoco/PitchPip.git
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+https://github.com/scrupycoco/PitchPip.git
 
 photos = UploadSet('photos',IMAGES)
 configure_uploads(app,photos)
+https://github.com/scrupycoco/PitchPip.git
 
 
 from Pitching import routes
