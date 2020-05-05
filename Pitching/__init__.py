cabb2c3ef429081561533a7dfe42e836c21e7e67
Pip Bbaale <namugera.philip@gmail.com>
2020-05-05 22:47:36 +0300

    _init_ configs

--- Pitching/__init__.py
+++ Pitching/__init__.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOADED_PHOTOS_DEST'] ='Pitching/static/photos'
-https://github.com/scrupycoco/PitchPip.git
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
