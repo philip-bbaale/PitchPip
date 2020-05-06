3fa038296dd3ce6c242b409ee63466c92346a28a
Pip Bbaale <namugera.philip@gmail.com>
2020-05-06 20:19:41 +0300

    adding stasch file

--- Pitching/routes.py
+++ Pitching/routes.py
@@ -167,7 +167,6 @@ def delete_post(post_id):
     return redirect(url_for('home'))
 
 @app.route("/posts_by_category/<uname>")
-@login_required
 def posts_by_category(uname):
     posts = Post.query.filter_by(category=uname).all()
     return render_template('posts_by_category', title='Posts By Category',  posts=posts)
